e68bbfd06cf00f60f53fd4068d3a6b643e8da9c5
Luke <mackenzieluke94@gmail.com>
2020-04-15 17:06:44 -0400

    published image-slider to npm

--- dev/src/pages/imageSlider.js
+++ dev/src/pages/imageSlider.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
-import ImageSlider from "@lmack/imageSlider"
+import ImageSlider from "@lmack/image-slider"
 
 const images = [
   "https://images.unsplash.com/photo-1449034446853-66c86144b0ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80",
